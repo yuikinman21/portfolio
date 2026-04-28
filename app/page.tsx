f25734f513a5263d09147c9aefe1ef6a5d26781b
yuikinman21 <yuikinman21@gmail.com>
2026-04-28 17:49:18 +0900

    無限スクロール領域のパディングを調整し、要素の間隔を改善

--- app/page.tsx
+++ app/page.tsx
@@ -542,7 +542,7 @@ export default function Home() {
           </div>
 
           {/* 無限スクロール領域 */}
-          <div className="mt-2 flex-1 flex flex-col justify-center gap-4 group-hover-pause mask-horizontal-fade min-h-0">
+          <div className="mt-2 flex-1 flex flex-col justify-center gap-2 group-hover-pause mask-horizontal-fade min-h-0 py-2">
             {/* 上段：LANGUAGE & FRAMEWORKS */}
             <div className="w-full relative py-3 -my-3">
               <div className="animate-scroll-left flex gap-4 px-2">
